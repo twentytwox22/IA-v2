b1c6ece3e517d98570c9fe3fe97ed427a552e5fc
junrui <limjrui@gmail.com>
2023-12-14 23:08:28 +1100

    made changes to controller files. added more columns to students table

--- src/student/controller.js
+++ src/student/controller.js
@@ -1,6 +1,8 @@
 const pool = require('../../db'); //22:22
 const queries = require('./queries'); //26:50
 
+//name, email, year, house, isRegistered, studentNumber
+
 const getStudents = (req, res) => { //20:25
    pool.query(queries.getStudents, (error, results) => { //27:15
     if (error) throw error; 
@@ -8,16 +10,16 @@ const getStudents = (req, res) => { //20:25
 });
 };
 
-const getStudentById = (req, res) => { 
+const getStudentByStudentNumber = (req, res) => { 
     const id = parseInt(req.params.id); //31:15
-    pool.query(queries.getStudentById, [id], (error, results) => {
+    pool.query(queries.getStudentByStudentNumber, [studentNumber], (error, results) => {
         if (error) throw error;
         res.status(200).json(results.rows); 
      }); //33:00
 };
 
 const addStudent = (req, res) => { //44:20
-    const {name, email, age, dob} = req.body; //36:30, destructuring 
+    const {name, email, year, house, isRegistered, studentNumber } = req.body; //36:30, destructuring 
     
     //check for duplicate email
    pool.query(queries.checkEmailExists, [email], (error, results) => {
@@ -25,7 +27,7 @@ const addStudent = (req, res) => { //44:20
         res.send("email already exists");
     } 
     //add student to db
-    pool.query(queries.addStudent, [name, email, age, dob], (error, results) => {
+    pool.query(queries.addStudent, [name, email, year, house, isRegistered, studentNumber], (error, results) => {
         if (error) throw error;
         res.status(201).send("student added");
         console.log('student added');
@@ -37,14 +39,14 @@ const addStudent = (req, res) => { //44:20
 const removeStudent = (req, res) => { //
     const id = parseInt(req.params.id); //51:45
     // check if student exists
-    pool.query(queries.getStudentById, [id], (error, results) => {
+    pool.query(queries.getStudentByStudentNumber, [studentNumber], (error, results) => {
         const noStudentFound = !results.rows.length; //53:40, if there are no results then there is no student found
         if (noStudentFound) {
         res.send('student does not exist in db, could not remove');
         } //54:15
 
         // remove student from db
-        pool.query(queries.removeStudent, [id], (error, results) => {
+        pool.query(queries.removeStudent, [studentNumber], (error, results) => {
             if (error) throw error;
             res.status(200).send('student removed');
             console.log('student removed');
@@ -57,13 +59,13 @@ const updateStudent = (req, res) => {
     const {name} = req.body;
 
     // check if student exists
-    pool.query(queries.getStudentById, [id], (error, results) => {
+    pool.query(queries.getStudentByStudentNumber, [studentNumber], (error, results) => {
         const noStudentFound =!results.rows.length; // if there are no results then there is no student found
         if (noStudentFound) {
         res.send('student does not exist in db, could not update');
         } //1:02:25
 
-        pool.query(queries.updateStudent, [name, id], (error, results) => {
+        pool.query(queries.updateStudent, [name, studentNumber], (error, results) => {
             if (error) throw error;
             res.status(200).send('student updated');
         }); //1:05:20
@@ -72,7 +74,7 @@ const updateStudent = (req, res) => {
 
 module.exports = {
     getStudents, //20:30
-    getStudentById,
+    getStudentByStudentNumber,
     addStudent,
     removeStudent,
     updateStudent,
--- src/student/queries.js
+++ src/student/queries.js
@@ -1,10 +1,10 @@
 const getStudents ="SELECT * FROM students"; //26:00
-const getStudentById ="SELECT * FROM students WHERE id = $1"; //26:40, where $1 is a varible name
+const getStudentByStudentNumber ="SELECT * FROM students WHERE studentNumber = $1"; //26:40, where $1 is a varible name
 const checkEmailExists = "SELECT s FROM students s WHERE s.email = $1"; //40:00
 const addStudent =
-"INSERT INTO students (name, email, age, dob) VALUES ($1,$2,$3,$4)"; //45:28
+"INSERT INTO students (name, email, year, house, isRegistered, studentNumber) VALUES ($1,$2,$3,$4,$5, $6)"; //45:28
 const removeStudent ="DELETE FROM students WHERE id = $1"; //55:15
-const updateStudent = "UPDATE students SET name = $1 WHERE id = $2"; //1:04:55
+const updateStudent = "UPDATE students SET name = $1 WHERE studentNumber = $2"; //1:04:55
 
 module.exports = {
  getStudents, //26:10
